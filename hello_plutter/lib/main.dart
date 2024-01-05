86627d1d24b52417eb5c497967039b7339938a3e
kyeongho <dagh1218@naver.com>
2024-01-05 21:08:08 +0900

    #15 Button 및 Text 부분 구현
    - div 대신 사용하는 Container widget 사용
    - 빈 요소로 widget간 간격 구성을 위한 SizedBox widget 사용
    - Padding 요소 구성을 위한 Padding widget 사용

--- hello_plutter/lib/main.dart
+++ hello_plutter/lib/main.dart
@@ -9,10 +9,12 @@ class RootWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Color(0xff181818),
-          body: Padding(
-            padding: EdgeInsets.all(30),
-            child: Column(children: [
+        backgroundColor: Color(0xff181818),
+        body: Padding(
+          padding: EdgeInsets.all(30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
@@ -37,9 +39,79 @@ class RootWidget extends StatelessWidget {
                     ],
                   )
                 ],
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Text(
+                "Total Balance",
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                "\$5 194 382",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Container(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 50,
+                      ),
+                      child: Text(
+                        "Transfer",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Container(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 50,
+                      ),
+                      child: Text(
+                        "Request",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white.withOpacity(0.9),
+                        ),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                  ),
+                ],
               )
-            ]),
-          )),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
