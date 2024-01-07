fd4076641a6e37c059f98d5827889231f635a303
kyeongho <dagh1218@naver.com>
2024-01-07 20:29:53 +0900

    #15 카드 별 위치 재구성 요소를 MoneyCard 내부에서 구성하도록 개선
    - Transform.translate()를 중복해서 사용하고 있기에 실제 사용부인 MoneyCard widget에서 처리하도록 개선

--- hello_plutter/lib/widgets/card.dart
+++ hello_plutter/lib/widgets/card.dart
@@ -6,6 +6,7 @@ class MoneyCard extends StatelessWidget {
   final String moneyCode;
   final IconData iconData;
   final bool isReversed;
+  final int order;
 
   final Color _baseColor = const Color.fromARGB(255, 29, 29, 29);
 
@@ -16,72 +17,76 @@ class MoneyCard extends StatelessWidget {
     required this.moneyCode,
     required this.iconData,
     required this.isReversed,
+    required this.order,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: isReversed ? Colors.white : _baseColor,
-        borderRadius: const BorderRadius.all(Radius.circular(25)),
-      ),
-      clipBehavior: Clip.hardEdge,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 20,
-          horizontal: 25,
+    return Transform.translate(
+      offset: Offset(0, (order * -20)),
+      child: Container(
+        decoration: BoxDecoration(
+          color: isReversed ? Colors.white : _baseColor,
+          borderRadius: const BorderRadius.all(Radius.circular(25)),
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  moneyType,
-                  style: TextStyle(
+        clipBehavior: Clip.hardEdge,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 20,
+            horizontal: 25,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    moneyType,
+                    style: TextStyle(
+                      color: isReversed ? _baseColor : Colors.white,
+                      fontSize: 38,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        moneyValue,
+                        style: TextStyle(
+                          color: isReversed ? _baseColor : Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        moneyCode,
+                        style: TextStyle(
+                          color: isReversed ? _baseColor : Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Transform.scale(
+                scale: 2.5,
+                child: Transform.translate(
+                  offset: const Offset(0, 10),
+                  child: Icon(
+                    iconData,
                     color: isReversed ? _baseColor : Colors.white,
-                    fontSize: 38,
+                    size: 70,
                   ),
                 ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      moneyValue,
-                      style: TextStyle(
-                        color: isReversed ? _baseColor : Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      moneyCode,
-                      style: TextStyle(
-                        color: isReversed ? _baseColor : Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Transform.scale(
-              scale: 2.5,
-              child: Transform.translate(
-                offset: const Offset(0, 10),
-                child: Icon(
-                  iconData,
-                  color: isReversed ? _baseColor : Colors.white,
-                  size: 70,
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
