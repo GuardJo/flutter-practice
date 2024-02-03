90d06b6fbd65984b4ab9142847a6e831d1c2feed
kyeongho <dagh1218@naver.com>
2024-02-03 19:29:45 +0900

    #21 웹툰별 상세 페이지에서 상세 정보 렌더링 구현
    - 앞서 구현한 ApiService 내 상세 정보 반환 메소드를 통해 반환 정보를 렌더링
        - 파라미터로 웹툰별 id 값이 필요하여 Stateful Widget으로 변경
        - constructor 시점에 초기화할 수 없기에 initState를 재정의하여 초기화 진행

--- webtoon_app/lib/screens/detail_screen.dart
+++ webtoon_app/lib/screens/detail_screen.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:webtoon_app/models/webtoon_detail.dart';
 import 'package:webtoon_app/models/webtoon_model.dart';
+import 'package:webtoon_app/services/api_service.dart';
 
-class WebtoonDetailScreen extends StatelessWidget {
+class WebtoonDetailScreen extends StatefulWidget {
   final WebtoonInfo webtoon;
+
   const WebtoonDetailScreen({
     super.key,
     required this.webtoon,
   });
+
+  @override
+  State<WebtoonDetailScreen> createState() => _WebtoonDetailScreenState();
+}
+
+class _WebtoonDetailScreenState extends State<WebtoonDetailScreen> {
+  late final Future<WebtoonDetail> webtoonDetail;
+
+  @override
+  void initState() {
+    super.initState();
+    webtoonDetail = WebtoonApiService.getWebtoonDetailById(widget.webtoon.id);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,40 +30,65 @@ class WebtoonDetailScreen extends StatelessWidget {
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Text(webtoon.title),
+        title: Text(widget.webtoon.title),
         surfaceTintColor: Colors.white,
         shadowColor: Colors.black,
         elevation: 2,
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Hero(
-                tag: webtoon.id,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.2),
-                        offset: const Offset(10, -10),
-                        blurRadius: 15,
-                      ),
-                    ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 100),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Hero(
+                  tag: widget.webtoon.id,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          offset: const Offset(10, -10),
+                          blurRadius: 15,
+                        ),
+                      ],
+                    ),
+                    clipBehavior: Clip.hardEdge,
+                    width: 300,
+                    child: Image.network(widget.webtoon.thumb),
                   ),
-                  clipBehavior: Clip.hardEdge,
-                  width: 300,
-                  child: Image.network(webtoon.thumb),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            FutureBuilder(
+              future: webtoonDetail,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  var data = snapshot.data!;
+                  return Column(
+                    children: [
+                      Text(data.about),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text("${data.age} / ${data.genre}")
+                    ],
+                  );
+                } else {
+                  return const Text("...");
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
